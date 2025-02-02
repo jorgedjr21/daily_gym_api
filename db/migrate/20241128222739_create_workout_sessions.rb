d2652810d40052b3b8a4e9d0416581281c1a46ba
Jorge Junior <jorgedjr21@gmail.com>
2025-02-01 23:59:28 -0300

    bump brakeman

--- db/migrate/20241128222739_create_workout_sessions.rb
+++ db/migrate/20241128222739_create_workout_sessions.rb
@@ -2,7 +2,6 @@ class CreateWorkoutSessions < ActiveRecord::Migration[7.2]
   def change
     create_table :workout_sessions do |t|
       t.string :name
-      t.references :workout_plan, null: true, foreign_key: true
 
       t.timestamps
     end
