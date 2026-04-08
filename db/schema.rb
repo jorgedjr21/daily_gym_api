aa62fcf6d9aedb1b6599cb8771df7d141e22db14
Jorge Junior <jorgedjr21@gmail.com>
2026-04-07 21:43:41 -0300

    fix seed typo

--- db/schema.rb
+++ db/schema.rb
@@ -10,44 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_15_154031) do
+ActiveRecord::Schema[8.1].define(version: 2025_02_15_154031) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "exercises", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
     t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_exercises_on_name", unique: true
   end
 
   create_table "jwt_blacklists", force: :cascade do |t|
-    t.string "jti"
     t.datetime "created_at", null: false
+    t.string "jti"
     t.datetime "updated_at", null: false
     t.index ["jti"], name: "index_jwt_blacklists_on_jti"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "role"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string "name", null: false
     t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.string "role"
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "workout_plan_sessions", force: :cascade do |t|
-    t.bigint "workout_plan_id", null: false
-    t.bigint "workout_session_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "workout_plan_id", null: false
+    t.bigint "workout_session_id", null: false
     t.index ["workout_plan_id", "workout_session_id"], name: "index_workout_plan_sessions_on_plan_id_and_session_id", unique: true
     t.index ["workout_plan_id"], name: "index_workout_plan_sessions_on_workout_plan_id"
     t.index ["workout_session_id", "workout_plan_id"], name: "index_workout_session_plans_on_session_id_and_plan_id", unique: true
@@ -55,30 +55,30 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_15_154031) do
   end
 
   create_table "workout_plans", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
     t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_workout_plans_on_user_id"
   end
 
   create_table "workout_session_exercises", force: :cascade do |t|
-    t.bigint "workout_session_id", null: false
-    t.bigint "exercise_id", null: false
-    t.integer "sets"
-    t.integer "reps"
-    t.string "technique"
-    t.decimal "current_weight"
     t.datetime "created_at", null: false
+    t.decimal "current_weight"
+    t.bigint "exercise_id", null: false
+    t.integer "reps"
+    t.integer "sets"
+    t.string "technique"
     t.datetime "updated_at", null: false
+    t.bigint "workout_session_id", null: false
     t.index ["exercise_id"], name: "index_workout_session_exercises_on_exercise_id"
     t.index ["workout_session_id"], name: "index_workout_session_exercises_on_workout_session_id"
   end
 
   create_table "workout_sessions", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_workout_sessions_on_user_id"
--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,4 +1,4 @@
 
 
 User.create(name: 'User', email: 'admin@email.com', password: '123456789', password_confirmation: '123456789', role: 'admin')
-puts "✅ Exercise seeds loaded successfully!"
+puts "✅ Users seeds loaded successfully!"
